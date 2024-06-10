57631c1b1a96275a3c1514336c8d67fb98a9fc0a
Alexandre dos Santos Sanches <sanches.ale.3018@gmail.com>
2024-06-10 16:33:07 -0300

    Instaciando via Scanner

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Conta {
+    private String nomeCliente;
+    private String agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public void inserirCliente() {
+        System.out.println("Digite o seu nome:");
+        String cliente = new Scanner(System.in).nextLine();
+        setNomeCliente(cliente);
+    }
+
+    public void inserirNumeroConta() {
+        System.out.println("Digite o número da sua conta:");
+        int numeroConta = new Scanner(System.in).nextInt();
+        setNumeroConta(numeroConta);
+
+    }
+
+    public void inserirAgencia() {
+        System.out.println("Digite sua agência:");
+        String agencia = new Scanner(System.in).nextLine();
+        setAgencia(agencia);
+
+    }
+
+    public void inserirSaldo() {
+        System.out.println("Digite seu Saldo:");
+        double saldo = new Scanner(System.in).nextDouble();
+        setSaldo(saldo);
+
+    }
+
+    public void retornoDados() {
+        System.out.println("Olá " + getNomeCliente() + "!");
+        System.out.println("Conta: " + getNumeroConta());
+        System.out.println("Agência: " + getAgencia());
+        System.out.println("Saldo: " + getSaldo());
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, olá  World!");
+        Conta cliente = new Conta();
+        cliente.inserirCliente();
+        cliente.inserirAgencia();
+        cliente.inserirNumeroConta();
+        cliente.inserirSaldo();
+        cliente.retornoDados();
+
     }
 }
